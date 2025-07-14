669f5ecaca45142edc156369cab13a9083ba4663
VladimirKBG <owl2666@mail.ru>
2025-07-14 20:17:30 +0500

    Тестовый коммит

---
+++ src/main/java/datsteam/datspulse/AntHillController/AntHillControllerApplication.java
@@ -0,0 +1,14 @@
+package datsteam.datspulse.AntHillController;
+
+//
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntHillControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntHillControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/datsteam/datspulse/AntHillController/AntHillControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package datsteam.datspulse.AntHillController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntHillControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
